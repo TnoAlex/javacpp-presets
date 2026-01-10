191f9aee73ac122e9c070b638092995ff4d87b36
TnoAlex <millstom2@outlook.com>
2026-01-10 10:49:55 +0800

    test

--- ffmpeg/cppbuild.sh
+++ ffmpeg/cppbuild.sh
@@ -108,6 +108,8 @@ if [[ "${ACLOCAL_PATH:-}" == C:\\msys64\\* ]]; then
     export ACLOCAL_PATH=/mingw64/share/aclocal:/usr/share/aclocal
 fi
 
+patch -Np1 -d nasm-$NASM_VERSION < ../../nasm.patch
+
 cd nasm-$NASM_VERSION
 # fix for build with GCC 8.x
 sedinplace 's/void pure_func/void/g' include/nasmlib.h
